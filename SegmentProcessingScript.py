2d4a713b9de0af68be3b0a81f80b383e7858a1c2
lesagejonathan <jonathan.lesage@gmail.com>
2017-07-27 15:29:39 -0400

    Edited SegmentProcessingScript.py to run on 10 Degree MP Scans

--- SegmentProcessingScript.py
+++ SegmentProcessingScript.py
@@ -1,5 +1,57 @@
 import SegmentAnalysis as sa
+import Signal
 import pickle
 import os
+import multiprocessing
 
-pth = '/home/jbox/Documents/LScans/'
+# def ImageWeld(x):
+#
+#     print(x)
+#
+#     wld = pickle.load(open(pth+x,'rb'))
+#
+#     F = sa.FMC(wld['AScans'],wld['SamplingFrequency'])
+#
+#     F.Calibrate()
+#
+#     F.FusionLineFocus()
+#
+#     return F.FusionLineImages
+#
+
+
+pth = '/home/jbox/Documents/MicroPulse-L-FBHScans-10DegreeWedge/'
+
+dref = pickle.load(open('/home/jbox/Documents/5L32-25mmReferenceBlock.p','rb'))
+pd = Signal.EstimateProbeDelays(dref['AScans'],dref['SamplingFrequency'],0.6,25.)
+
+d = os.listdir(pth)
+
+d = [dd for dd in d if dd.endswith('.p')]
+
+def ImageWeld(x):
+
+    print(x)
+
+    wld = pickle.load(open(pth+x,'rb'))
+
+    F = sa.FMC(wld['AScans'],wld['SamplingFrequency'])
+
+    F.Calibrate(ProbeDelays=pd)
+
+    F.FusionLineFocus()
+
+    return F.FusionLineImages
+
+
+pool = multiprocessing.Pool(multiprocessing.cpu_count())
+II = pool.map(ImageWeld, d)
+
+I = {}
+
+for i in range(len(d)):
+
+    I[dd.strip('.p')] = II[i]
+
+
+pickle.dump(I,open('/home/jbox/Documents/CustomTFM-MicroPulse-L-FBH-10DegreeWedge.p','wb'))
