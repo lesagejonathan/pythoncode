74c1b219e089423ab3b19157429620c64e70352f
lesagejonathan <jonathan.lesage@gmail.com>
2018-05-08 10:43:46 -0400

    Added PAUT.py CalculateOffsets method and changed Sweep class to support wedge

--- PAUT.py
+++ PAUT.py
@@ -1,11 +1,26 @@
 import numpy as np
-from numpy.fft import *
+from numpy.fft import rfft, fftshift, ifft, ifftn, ifftshift, irfft
 from Signal import ShiftSignal
+
+def CalculateOffsets(angles, Tdelay, cw = 2.33, cs = 3.24):
+
+    """ Calculates index offsets starting from first angle in angles as zero
+        offset, using wedge and sample velocities cw and cs, refracted angles and wedge delay for
+        first angle Tdelay """
+
+    angles = angles*np.pi/180.
+    anglesw = np.arcsin(cw*np.sin(angles)/cs)
+
+    h = 0.5*cw*Tdelay*np.cos(anglesw[0])
+
+    Offsets = h*np.tan(anglesw)
+
+    return Offsets
 
 
 class Sweep:
 
-    def __init__(self, scans, samplingfreq, angles, pitch, numberofelements, c, indexoffsets=None):
+    def __init__(self, scans, samplingfreq, angles, pitch, numberofelements, c, indexoffsets=None, rangeoffsets=None):
 
         from copy import deepcopy
 
@@ -18,49 +33,42 @@ class Sweep:
         self.WaveSpeed = c
 
         self.IndexOffsets = indexoffsets
+        self.RangeOffsets = rangeoffsets
+
+        if self.IndexOffsets is not None:
+
+            self.x0 = self.IndexOffsets
+
+        if self.RangeOffsets is not None:
+
+            self.x0 = self.x0 + self.RangeOffsets*np.sin(self.Angles)
+
+            self.x0 = self.x0 - 0.5*(np.amin(self.x0)+np.amax(self.x0))
+            self.y0 = self.RangeOffsets*np.cos(self.Angles)
 
 
+    def FWtoRF(self,fc,BW):
+
+        """ Uses centre frequency fc, and fractional bandwidth BW, to approximately
+            undo full wave rectification and reproduce estimate of original RF signal.
+            Modifies self.Scans """
 
 
+        sigma = fc*BW/(2*np.sqrt(2*np.log(2)))
 
-        # if type(c) is float:
-        #
-        #     self.WaveSpeed = {'Wedge': c, 'Piece': c}
-        #     self.IndexOffsets = np.zeros(len(self.Angles))
-        #
-        # elif type(c) is dict:
-        #
-        #     self.WaveSpeed = c
-        #     self.IndexOffsets = indexoffsets
+        Nt = self.Scans[0].shape[1]
 
+        t = np.linspace(0.,Nt-1,Nt)/self.SamplingFrequency
+
+        f = np.linspace(0., self.SamplingFrequency/2, int(np.floor(Nt/2) + 1))
+
+        H = np.exp(-(f/(2*sigma))**2).reshape(1,-1)
 
 
-    def ZeroTime(self):
+        h = np.cos(2*np.pi*fc*t).reshape(1,-1)
 
-        """ Moves the zero time position to the centre of the aperture for all
-        scans """
 
-        L = self.Scans[0].shape[1]
-
-        for i in range(len(self.Scans)):
-
-            for n in range(len(self.Angles)):
-
-                self.Scans[i][n,:] = ShiftSignal(self.Scans[i][n,:],-(self.NumberOfElements-1)*self.Pitch*np.sin(np.abs(self.Angles[n]))/self.WaveSpeed['Wedge'], self.SamplingFrequency)
-
-                # d = int(np.round(self.SamplingFrequency*(self.NumberOfElements - 1)*self.Pitch*np.sin(np.abs(self.Angles[n]))/self.WaveSpeed['Wedge']))
-
-                # print(d)
-                #
-                # print(self.Scans[i][n].shape)
-                #
-                # print(self.Scans[i][n,d::].shape)
-
-                # s = np.zeros((len(self.Angles) , L))
-                #
-                # s[n,0:(L-d)] = self.Scans[i][n,d::]
-
-                # self.Scans[i][n,:] = np.concatenate((self.Scans[i][n,d::],self.Scans[i][n,-1]*np.ones(d)))
+        self.Scans = [irfft(rfft(s)*H)*h for s in self.Scans]
 
 
 
@@ -70,16 +78,28 @@ class Sweep:
 
         c = self.WaveSpeed
 
-        a = self.Scans[ScanIndex]
+        a = np.abs(self.Scans[ScanIndex])
 
         r = np.linspace(0.,c*a.shape[1]/(2*self.SamplingFrequency), a.shape[1]).reshape(1,-1)
 
         ang = self.Angles.reshape(-1,1)
 
-        x = r*np.sin(ang)
-    # y = r/np.cos(angrad)
+        if self.IndexOffsets is None:
 
-        y = r*np.cos(ang)
+            x = r*np.sin(ang)
+
+
+            # y = r/np.cos(ang)
+
+            y = r*np.cos(ang)
+
+        else:
+
+            x = r*np.sin(ang) + self.x0.reshape(-1,1)
+
+            # y = r/np.cos(ang) + self.y0.reshape(-1,1)
+
+            y = r*np.cos(ang) + self.y0.reshape(-1,1)
 
         xmin = np.min(x)
         xmax = np.max(x)
@@ -93,95 +113,23 @@ class Sweep:
 
         xygrid = np.meshgrid(xgrid,ygrid)
 
-        I = griddata((x.flatten(), y.flatten()), a.flatten(), (xygrid[0].flatten(), xygrid[1].flatten()), method='cubic', fill_value=0., rescale = True)
+        I = griddata((x.flatten(), y.flatten()), a.flatten(), (xygrid[0].flatten(), xygrid[1].flatten()), method='linear', fill_value=0., rescale = False)
 
 
         I = I.reshape(xygrid[0].shape)
 
-    # I[I>100.]=100.
-
-    # I =
-
-        return I
-
-#
-
-    def GetBackPropagationImage(self, ScanIndex, L, fband, Resolution):
-
-        # from scipy.interpolate import interp1d
-
-        c = self.WaveSpeed
-
-        A = rfft(self.Scans[ScanIndex])
-
-        # print(a.shape)
-
-        # print(A.shape)
-
-        f = np.linspace(0.,self.SamplingFrequency/2,A.shape[-1]).reshape(1,-1)
-
-        indf = (f>=fband[0])&(f<=fband[1])
-        indf = np.array(indf).flatten()
-
-        ang = self.Angles.reshape(-1,1)
+        return I, (xmin,xmax,ymax,ymin)
 
 
-        # print(indf.shape)
 
-        A = A[:,indf]
-
-        A = A/np.cos(ang)
-
-        w = 2*np.pi*f[:,indf]
-
-        dy = c/(2*fband[1])
-
-        Ny = int(np.round(L[1]/(2*dy)))
-
-        kygrid = 2*np.pi*np.linspace(0,1/(2*dy), Ny)
-
-        ky = 2*w*np.cos(ang)/self.WaveSpeed['Piece']
-
-        II = np.array([np.interp(kygrid, ky[i,:], A[i,:], 0+0j, 0+0j) for i in range(A.shape[0])])
-
-        Ny = int(np.round(L[1]/(2*Resolution)))
-
-        Nx = int(np.round(L[0]/Resolution))
-
-        II = ifft(II,n=2*Ny - 2)
-
-        y = np.linspace(0.,L[1],II.shape[1])
-
-        x = y.reshape(1,-1)*np.tan(ang)
-
-        xgrid = np.linspace(-L[0]/2, L[0]/2, Nx)
-
-        print(y.shape)
-
-        print(x.shape)
-        print(II.shape)
-
-        I = np.abs(np.array([np.interp(xgrid, x[:,i], II[:,i], 0.+0j, 0.+0j) for i in range(II.shape[1])]))
-
-        # Nx = int(np.round(L[0]/Resolution))
-
-        # return np.abs(ifftn(fftshift(I,axes=(1,)),axes=(1,0),s=(Nx,2*Ny - 2)))
-
-        return I
-
-    # return np.abs(ifftn(I, s=(2*Ny - 2, Ny), axes=(1,0)))
-
-    def GetWavenumberImage(self, ScanIndex, L, fband, Resolution):
+    def GetWavenumberImage(self, ScanIndex, L, fband, Resolution, Npad=4):
 
         from scipy.interpolate import griddata
 
         c = self.WaveSpeed
 
-        A = rfft(self.Scans[ScanIndex], 4*self.Scans[ScanIndex].shape[1])
+        A = rfft(self.Scans[ScanIndex], int(Npad*self.Scans[ScanIndex].shape[1]))
 
-        # print(a.shape)
-
-        # print(A.shape)
 
         f = np.linspace(0.,self.SamplingFrequency/2,A.shape[-1]).reshape(1,-1)
 
@@ -189,7 +137,6 @@ class Sweep:
         indf = (f>=fband[0])&(f<=fband[1])
         indf = np.array(indf).flatten()
 
-        # print(indf.shape)
 
         A = A[:,indf]
 
@@ -199,72 +146,122 @@ class Sweep:
         angrad = self.Angles.reshape(-1,1)
 
 
-        kx = 2.*w*np.sin(angrad)/c
+        kx = 2.*w*np.sin(angrad)/c + 0j
 
-        ky = 2.*np.sqrt((w/c)**2 - (kx/2)**2 + 0+0j)
+        ky = 2.*w*np.cos(angrad)/c + 0j
 
-        ky = ky[np.abs(np.real(ky))>=0.]
+        # ky = 2.*np.sqrt((w/c)**2 - (kx/2)**2 +0j)
+
+        # print(kx.shape)
+
+        # print(self.x0.reshape(-1,1).shape)
 
 
-        # ky = 2*w*np.cos(angrad)/c
-
-
-        # AA = A/np.cos(angrad)
-
-        Nx = int(np.round(L[0]/self.Pitch))
-
-        dy = c/(2*fband[1])
-
-        Ny = int(np.round(L[1]/(2*dy)))
 
         if self.IndexOffsets is None:
 
 
+            Nx = int(np.round(L[0]/self.Pitch))
+
             kxgrid = 2*np.pi*np.linspace(-1/(2*self.Pitch),1/(2*self.Pitch), Nx)
 
-
-            kygrid = 2*np.pi*np.linspace(0,1/(2*dy), Ny)
-
-            kgrid = np.meshgrid(kxgrid, kygrid)
-
-            I = griddata((kx.flatten(), ky.flatten()), A.flatten(), (kgrid[0].flatten(),kgrid[1].flatten()), method = 'cubic', fill_value=0+0j, rescale=True).reshape(kgrid[0].shape)
-
-                # I = I*np.exp(-1j*kgrid[0]*L[0])
-
-            Nx = int(np.round(L[0]/Resolution))
-
-            xzeropad = np.zeros((I.shape[0],int(np.round((Nx - I.shape[1]-2)/2))), dtype=complex)
-
-                # I = fftshift(ifft(ifftshift(np.hstack((xzeropad, I, xzeropad)), axes=(1,))), axes=(1,))
-
-            I = ifft(ifftshift(np.hstack((xzeropad, I, xzeropad)), axes=(1,)))
-
-
-            Ny = int(np.round(L[1]/(2*Resolution)))
-
-                # return np.abs(ifftn(fftshift(I,axes=(1,)),axes=(1,0),s=(Nx,2*Ny - 2)))
-
-            return fftshift(ifft(I,axis=0,n=2*Ny - 2),axes=(1,))
 
 
         else:
 
-            kxgrid = 2*np.pi*np.linspace(0.,1/(2*self.Pitch), Nx)
+            A = A*np.exp(-1j*kx*self.x0.reshape(-1,1))*np.exp(-1j*ky*self.y0.reshape(-1,1))
 
-            xx = np.abs(self.IndexOffsets)
+            p = np.amin(np.diff(self.x0))
 
-            xx = (xx-np.amax(xx)).reshape(kx.shape)
+            p = np.pi/np.amax(np.abs(kx))
 
-            A = A*np.exp(-2j*kx*xx)
+            # p = self.Pitch
 
-            kygrid = 2*np.pi*np.linspace(0,1/(2*dy), Ny)
+            Nx = int(np.round(L[0]/p))
 
-            kgrid = np.meshgrid(kxgrid, kygrid)
 
-            I = griddata((kx.flatten(), ky.flatten()), A.flatten(), (kgrid[0].flatten(),kgrid[1].flatten()), method = 'cubic', fill_value=0+0j, rescale=True).reshape(kgrid[0].shape)
+            kxgrid = 2*np.pi*np.linspace(-1/(2*p), 1/(2*p), Nx)
 
-            Nx = int(np.round(L[0]/(2*Resolution)))
+            # kxgrid = 2*np.pi*np.linspace(-1/(2*self.Pitch),1/(2*self.Pitch), Nx)
 
-            Ny = int(np.round(L[1]/(2*Resolution)))
 
-            return ifftn(I,axes=(0,1),s=(2*Nx - 2, 2*Ny - 2))
+        # print(np.amin(np.diff(self.x0)))
+
+
+        # ky = ky[np.abs(np.real(ky))>=0.]
+
+        # kxmax = np.amax(np.abs(kx))/2
+        #
+        # dx = np.pi/kxmax
+        #
+        # print(dx)
+        #
+        #
+        # Nx = int(np.round(L[0]/dx))
+        #
+        # # kxgrid = 2*np.pi*np.linspace(-1/(2*dx), 1/(2*dx), Nx)
+        #
+        # kxgrid = np.linspace(-kxmax,kxmax, Nx)
+
+
+
+
+        # Nx = int(np.round(L[0]/self.Pitch))
+
+        # dy = c/(2*fband[1])
+
+        dy = np.pi/np.amax(np.abs(ky))
+
+        # print(dy)
+
+        Ny = int(np.round(L[1]/(2*dy)))
+
+
+
+        # kxgrid = 2*np.pi*np.linspace(-1/(2*self.Pitch),1/(2*self.Pitch), Nx) + 0j
+
+
+        kygrid = 2*np.pi*np.linspace(0,1/(2*dy), Ny) + 0j
+
+        kgrid = np.meshgrid(kxgrid, kygrid)
+
+
+
+        I = griddata((kx.flatten(), ky.flatten()), A.flatten(), (kgrid[0].flatten(),kgrid[1].flatten()), method = 'linear', fill_value=0+0j, rescale=False).reshape(kgrid[0].shape)
+
+
+        Nx = int(np.round(L[0]/Resolution))
+
+        xzeropad = np.zeros((I.shape[0],int(np.round((Nx - I.shape[1]-2)/2))), dtype=complex)
+
+
+        I = ifft(ifftshift(np.hstack((xzeropad, I, xzeropad)), axes=(1,)))
+
+
+        Ny = int(np.round(L[1]/(2*Resolution)))
+
+
+        return fftshift(ifft(I,axis=0,n=2*Ny - 2),axes=(1,))
+
+
+        # else:
+        #
+        #     kxgrid = 2*np.pi*np.linspace(0.,1/(2*self.Pitch), Nx)
+        #
+        #     xx = np.abs(self.IndexOffsets)
+        #
+        #     xx = (xx-np.amax(xx)).reshape(kx.shape)
+        #
+        #     A = A*np.exp(-2j*kx*xx)
+        #
+        #     kygrid = 2*np.pi*np.linspace(0,1/(2*dy), Ny)
+        #
+        #     kgrid = np.meshgrid(kxgrid, kygrid)
+        #
+        #     I = griddata((kx.flatten(), ky.flatten()), A.flatten(), (kgrid[0].flatten(),kgrid[1].flatten()), method = 'cubic', fill_value=0+0j, rescale=True).reshape(kgrid[0].shape)
+        #
+        #     Nx = int(np.round(L[0]/(2*Resolution)))
+        #
+        #     Ny = int(np.round(L[1]/(2*Resolution)))
+        #
+        #     return ifftn(I,axes=(0,1),s=(2*Nx - 2, 2*Ny - 2))
